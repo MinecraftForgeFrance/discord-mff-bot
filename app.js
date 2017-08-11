fe3e905d3a260f44d606f038fdfb2b3a31de5326
Superloup10 <Superloup10@gmail.com>
2017-08-11 17:37:09 +0200

    Add command register

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 const Discord = require('discord.js');
-const request = require('request');
 const client = new Discord.Client();
 const token = '***REMOVED***';
 
-let username = "";
-
+let userLastCommand = [];
 client.on("ready", () => {
     console.log('I am ready!');
 });
 
-client.on("message", message =>{
-    if(message.channel.type === 'dm') {
-        if(!message.author.bot) {
-            username = message.content;
-            console.log(`username : ${username}`);
-            request.post({
-                url: "http://localhost/mybb/inc/plugins/minecraftforgefrance.php",
-                form: {
-                    username: message.content
-                }
-            }, function(error, response, body){
-                console.log(body);
-            });
-            message.channel.send("Votre message a bien été reçu, il sera traité dès que possible.")
-                .then(async message => console.log(`Send message: ${message.content}`))
-                .catch(console.error());
+
+client.on("message", message => {
+    if (!message.author.bot) {
+        let messageUser = message.author.id;
+
+        if (message.content.indexOf('!') !== 0 && !(typeof(userLastCommand[messageUser]) === 'string')) return;
+        if (message.content.indexOf('!') !== 0)
+        // use user id instead of username
+            message.content = `!${userLastCommand[messageUser]} ${message.content}`;
+        // This is the best way to define args. Trust me.
+        const args = message.content.slice(1).trim().split(/ +/g);
+        const command = args.shift().toLowerCase();
+        userLastCommand[messageUser] = command;
+        // The list of if/else is replaced with those simple 2 lines:
+        try {
+            let commandFile = require(`./commands/${message.channel.type}_${command}.js`);
+            if (commandFile.canRun(client, messageUser)) {
+                commandFile.run(client, messageUser, message, args);
+            }
+        } catch (err) {
+            console.error(err);
         }
     }
 });
 
 client.on("guildMemberAdd", member => {
-    member.send("Bonjour, " + member.displayName + "\nMerci de m\'indiquer votre pseudo sur le forum.")
+    /*
+        Messages :
+        - Bienvenu sur le discord de mff
+        - pour aquérir vos droits enregistrez vous / confirmer membre forum
+        - taper '!register pseudo'
+    */
+
+    member.send(`Bonjour **${member.displayName}**,\nPour acquérir vos droits sur le Discord, merci de m'indiquer votre pseudo sur le forum à l'aide de la commande \`!register "pseudo"\`.`)
         .then(async (message) => console.log(`Sent message: ${message.content}`))
         .catch(console.error());
 });
---
+++ commands/dm_register.js
@@ -0,0 +1,88 @@
+const jsonFile = require('jsonfile');
+const request = require('request');
+
+let username = "";
+let attempt = 3;
+let users = {
+    data: {}
+};
+
+module.exports = {
+    run: (client, messageUser, message, args) => {
+        // Read file before process message
+        users = jsonFile.readFileSync("data/users.json");
+
+        if (typeof(users.data[messageUser]) === "undefined" || (typeof(users.data[messageUser]) === "object" && users.data[messageUser].step === 0)) {
+            username = args[0];
+            console.log(`username : ${username}`);
+            // test username already use
+            for (const user in users.data) {
+                if (user.username === username) {
+                    return;
+                }
+            }
+
+            // generate token and add user to file
+            request(`http://localhost/mybb/inc/plugins/minecraftforgefrance.php?username=${username}`, (err, res, body) => {
+                if (body === "")
+                    message.channel.send(":x: Votre pseudo n'existe pas ou n'est pas correct.");
+                else {
+                    // save user infos
+                    users.data[messageUser] = {
+                        "username": username,
+                        "token": body,
+                        "step": 1,
+                        "attempt": attempt
+                    };
+                    // save node
+                    jsonFile.writeFile("data/users.json", users, {spaces: 4}, err => {
+                        if (err)
+                            throw err;
+                        console.log("This file has been saved");
+                    });
+                    console.log(body);
+
+                    message.channel.send(":white_check_mark: Un code vient de vous être envoyé par mp, veuillez l'indiquer en réponse à ce message.");
+                }
+            });
+        } else if (users.data[messageUser].step === 1) {
+            // wait for token
+            if (users.data[messageUser].attempt !== 0 && args[0] !== users.data[messageUser].token) {
+                users.data[messageUser].attempt--;
+                if (users.data[messageUser].attempt === 0)
+                    message.channel.send(":no_entry: **Code incorrect. Vous avez épuisé vos 3 essais.**\nVeuillez contacter l'équipe de Minecraft Forge France pour obtenir vos droits sur le Discord.").catch(console.error());
+                else {
+                    message.channel.send(`:x: **Code incorrect, veuillez réessayer. **Nombre d\'essai restant : **${users.data[messageUser].attempt}**`);
+                    jsonFile.writeFile("data/users.json", users, {spaces: 4}, err => {
+                        if (err)
+                            throw err;
+                        console.log("This file has been saved");
+                    });
+                }
+            } else {
+                message.channel.send(":white_check_mark: **Code valide. Bienvenue !**\nPrière de lire les règles du salon #regles sur le Discord.").catch(console.error());
+                users.data[messageUser].step = 2;
+                // save node
+                jsonFile.writeFile("data/users.json", users, {spaces: 4}, err => {
+                    if (err)
+                        throw err;
+                    console.log("This file has been saved");
+                });
+
+                for (const guild of client.guilds) {
+                    let role = guild[1].roles.find("name", "Membre");
+                    guild[1].members.get(messageUser).addRole(role).then(console.log("Success !")).catch(console.error);
+                }
+            }
+        }
+    },
+    canRun: (client, messageUser) => {
+        // Read file before process message
+        users = jsonFile.readFileSync("data/users.json");
+
+        if (typeof(users.data[messageUser]) === "object" && users.data[messageUser].step === 2)
+            return false;
+        else
+            return true;
+    }
+};
