03410ea60bb10997fda724e78a6dbb7830173a68
Superloup10 <superloup10@gmail.com>
2017-08-21 23:23:08 +0200

    Add ban command

--- commands/text_ban.js
+++ commands/text_ban.js
@@ -14,57 +14,38 @@ module.exports = {
 
      */
     run: (client, messageUser, message, args) => {
+        banList = jsonFile.readFileSync();
         let member = message.mentions.members.first();
         let time, reason;
-        if (/^(\d)+([mhdMY])$/.test(args[1])) {
-            time = /^(\d)+([mhdMY])$/.exec(args[1]); // optional
-            let year = 0, month = 0, day = 0, hour = 0, minute = 0;
-            switch (time[2]) {
-                case 'm':
-                    // m > h > d > M > Y
-                    minute = time[1] % 60;
-                    hour = time[1] / 60;
-                    day = hour / 24;
-                    hour %= 24;
-                    month = day / 30;
-                    day %= 30;
-                    year = month / 12;
-                    month %= 12;
-                    break;
-                case 'h':
-                    // h > d > M > Y
-                    hour = time[1] % 24;
-                    day = time[1] / 24;
-                    month = day / 30;
-                    day %= 30;
-                    year = month / 12;
-                    month %= 12;
-                    break;
-                case 'd':
-                    // d > M > Y
-                    day = time[1] % 30;
-                    month = time[1] / 30;
-                    year = month / 12;
-                    month %= 12;
-                    break;
-                case 'M':
-                    month = time[1] % 12;
-                    year = time[1] / 12;
-                    break;
-                case 'Y':
-                    year = time[1];
-                    break;
-            }
-            time = moment().add(minute, 'm').add(hour, 'h').add(day, 'd').add(month, 'M').add(year, 'Y');
+        if (/^(\d+)([mhdMY])$/.test(args[1])) {
+            time = /^(\d+)([mhdMY])$/.exec(args[1]); // optional
+            moment.locale('fr');
+            time = moment().add(time[1], time[2]);
             reason = message.content.split(/\s+/g).slice(3).join(" ");
-            console.log('ban temp: ' + time.toString());
+            message.channel.send(`${member} a été banni jusqu'au ${time.format('dddd D MMMM YYYY à kk:mm')}.`)
+                .then(async (message) => console.log(`Send message : ${message.content}`))
+                .catch(console.error());
         } else {
-            time = "def";
             reason = message.content.split(/\s+/g).slice(2).join(" ");
-            console.log('ban def!');
+            time = 'def';
+            message.channel.send(`${member} a été banni définitivement.`)
+                .then(async (message) => console.log(`Send message : ${message.content}`))
+                .catch(console.error());
         }
 
-// !ban @isador <1m|1h|1d|1M|1Y> [reason]
+        banList.data[member.id] = {
+            dateNow: Date.now(),
+            operator: messageUser,
+            reason: reason,
+            endBan: time
+        };
+
+        jsonFile.writeFile("data/ban.json", banList, {spaces: 4}, err => {
+            if (err)
+                throw err;
+            console.log("This file has been saved");
+        });
+        member.ban(reason);
     },
     canRun: (client, messageUser, message) => {
         const role = message.guild.roles.find("name", defaultConfig.roles.roleAdmin);
