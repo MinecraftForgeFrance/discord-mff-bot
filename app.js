244f99e572a4b583b0a30fec6f12872f53849eb2
Superloup10 <superloup10@gmail.com>
2017-09-01 15:17:48 +0200

    Add filterMessage for nsfw channel

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 const config = require("./config/config.js");
 const schedule = require("node-schedule");
 const jsonFile = require('jsonfile');
+const fs = require("fs");
 
 // Config
 const defaultConfig = process.env.NODE_ENV === 'production' ? config.readConfig() : config.defaultConfig();
@@ -43,10 +44,14 @@ client.on("ready", () => {
     });
 });
 
+client.on("messageUpdate", (oldMessage, newMessage) => {
+    filterMessage(newMessage);
+});
+
 client.on("message", message => {
     if (!message.author.bot) {
         let messageUser = message.author.id;
-
+        filterMessage(message);
         //if (message.content.indexOf('!') !== 0 && !(typeof(userLastCommand[messageUser]) === 'string')) return;
         if (message.content.indexOf(defaultConfig.bot.prefix) !== 0) return;
         // use user id instead of username
@@ -57,13 +62,19 @@ client.on("message", message => {
         //userLastCommand[messageUser] = command;
         // The list of if/else is replaced with those simple 2 lines:
         try {
-            let commandFile = require(`./commands/${message.channel.type}_${command}.js`);
-            //if exists
-            if (commandFile.canRun(client, messageUser, message)) {
-                commandFile.run(client, messageUser, message, args);
-            }
-            else {
-                message.reply("la commande ne peut pas s'exécuter")
+            if (fs.existsSync(`./commands/${message.channel.type}_${command}.js`)) {
+
+                let commandFile = require(`./commands/${message.channel.type}_${command}.js`);
+                if (commandFile.canRun(client, messageUser, message)) { // and time between two command > 10s /// users[userID] = sendTime
+                    commandFile.run(client, messageUser, message, args);
+                }
+                else {
+                    message.reply("la commande ne peut pas s'exécuter")
+                        .then(async (message) => console.log(`Sent message: ${message.content}`))
+                        .catch(console.error());
+                }
+            } else {
+                message.reply("la commande n'existe pas")
                     .then(async (message) => console.log(`Sent message: ${message.content}`))
                     .catch(console.error());
             }
@@ -101,5 +112,23 @@ client.on("guildMemberRemove", member => {
             .catch(console.error());
     }
 });
+
+function filterMessage(message) {
+    if (message.channel.nsfw) {
+        if (message.attachments.array().length !== 0 || message.embeds.length !== 0 || /(<)?http(s)?:\/\/(.*)\.(.*)(>)?/.test(message.content)) {
+            message.delete()
+                .then(async (message) => console.log(`Delete message: ${message.author}`))
+                .catch(console.error());
+            message.reply("merci de ne pas poster d'image ou de vidéo dans ce channel sous peine de sanction")
+                .then(async (message) => console.log(`Sent message: ${message.content}`))
+                .catch(console.error());
+        }
+    }
+}
+
+function filterTimeMessage(message) {
+
+}
+
 
 client.login(defaultConfig.bot.token);
--- commands/text_modhelp.js
+++ commands/text_modhelp.js
@@ -5,76 +5,78 @@ const defaultConfig = process.env.NODE_ENV === 'production' ? config.readConfig(
 
 module.exports = {
     run: (client, messageUser, message, args) => {
-        if (message.channel.name === defaultConfig.channels.supportModding || message.channel.name === defaultConfig.channels.bots) {
-            if (args.length === 0)
-                message.channel.send("La syntaxe doit être `!modhelp \<\sujet\>`")
-                    .then(async (message) => console.log(`Send message : ${message.content}`))
-                    .catch(console.error());
-            else {
-                let supportName = "";
-                for (let i = 0; i < args.length; i++)
-                    supportName += (i === (args.length - 1)) ? args[i] : args[i] + " ";
+        if (args.length === 0)
+            message.channel.send("La syntaxe doit être `!modhelp \<\sujet\>`")
+                .then(async (message) => console.log(`Send message : ${message.content}`))
+                .catch(console.error());
+        else {
+            let supportName = "";
+            for (let i = 0; i < args.length; i++)
+                supportName += (i === (args.length - 1)) ? args[i] : args[i] + " ";
 
-                request({
-                    uri: `${defaultConfig["protocol"]}://${defaultConfig["hostname"]}:${defaultConfig["port"]}${defaultConfig["path"]}?supportname=${supportName}&token=k3K0DnQaQMemIDAGnVP6`,
-                    json: true
-                }, (err, res, body) => {
-                    if (body === "Tutorial not found")
-                        message.channel.send("Il n'existe aucun tutoriel avec ce sujet")
-                            .then(async (message) => console.log(`Send message : ${message.content}`))
-                            .catch(console.error());
-                    else {
-                        let embed = new Discord.RichEmbed();
-                        embed.setColor(0xBD8D46).setTitle("Liste des sujets résolus correspondants à votre recherche : ");
-                        embed.setThumbnail("https://cdn.discordapp.com/attachments/270667098143981589/347773487093383189/avatar_128x128_transparent.png");
-                        let prefixArray = [];
-                        let fieldContent = [];
-                        if (typeof (body.prefix) !== "undefined") {
-                            for (let i = 0, j = 0; i < body.prefix.length; i++) {
-                                let field = `- [${body.subject[i]}](${defaultConfig["protocol"]}://${defaultConfig["hostname"]}:${defaultConfig["port"]}${defaultConfig["path2"]}?tid=${body.tid[i]})`;
-                                if (!prefixArray.includes(body.prefix[i])) {
+            request({
+                uri: `${defaultConfig["protocol"]}://${defaultConfig["hostname"]}:${defaultConfig["port"]}${defaultConfig["path"]}?supportname=${supportName}&token=k3K0DnQaQMemIDAGnVP6`,
+                json: true
+            }, (err, res, body) => {
+                if (body === "Tutorial not found")
+                    message.channel.send("Il n'existe aucune demande d'aide résolu avec ce sujet")
+                        .then(async (message) => console.log(`Send message : ${message.content}`))
+                        .catch(console.error());
+                else {
+                    let embed = new Discord.RichEmbed();
+                    embed.setColor(0xBD8D46).setTitle("Liste des sujets résolus correspondants à votre recherche : ");
+                    embed.setThumbnail("https://cdn.discordapp.com/attachments/270667098143981589/347773487093383189/avatar_128x128_transparent.png");
+                    let prefixArray = [];
+                    let fieldContent = [];
+                    if (typeof (body.prefix) !== "undefined") {
+                        for (let i = 0, j = 0; i < body.prefix.length; i++) {
+                            let field = `- [${body.subject[i]}](${defaultConfig["protocol"]}://${defaultConfig["hostname"]}:${defaultConfig["port"]}${defaultConfig["path2"]}?tid=${body.tid[i]})`;
+                            if (!prefixArray.includes(body.prefix[i])) {
+                                prefixArray[j] = body.prefix[i];
+                                fieldContent[j] = field;
+                                j++;
+                            }
+                            else {
+                                if (fieldContent[prefixArray.lastIndexOf(body.prefix[i])].length <= (1024 - field.length)) {
+                                    fieldContent[prefixArray.lastIndexOf(body.prefix[i])] += `\n${field}`;
+                                }
+                                else {
                                     prefixArray[j] = body.prefix[i];
                                     fieldContent[j] = field;
                                     j++;
                                 }
-                                else {
-                                    if (fieldContent[prefixArray.lastIndexOf(body.prefix[i])].length <= (1024 - field.length)) {
-                                        fieldContent[prefixArray.lastIndexOf(body.prefix[i])] += `\n${field}`;
-                                    }
-                                    else {
-                                        prefixArray[j] = body.prefix[i];
-                                        fieldContent[j] = field;
-                                        j++;
-                                    }
-                                }
-                            }
-
-                            if (prefixArray.length >= 25) {
-                                message.reply("votre recherche renvoit trop de résultat, merci de l'affiner.")
-                                    .then(async (message) => console.log(`Send message : ${message.content}`))
-                                    .catch(console.error());
-                            }
-                            else {
-                                for (let i = 0; i < prefixArray.length; i++) {
-                                    embed.addField(prefixArray[i], fieldContent[i]);
-                                }
-                                message.channel.send({embed}).catch(console.error());
                             }
                         }
+
+                        if (prefixArray.length >= 25) {
+                            message.reply("votre recherche renvoit trop de résultat, merci de l'affiner.")
+                                .then(async (message) => console.log(`Send message : ${message.content}`))
+                                .catch(console.error());
+                        }
+                        else {
+                            for (let i = 0; i < prefixArray.length; i++) {
+                                embed.addField(prefixArray[i], fieldContent[i]);
+                            }
+                            message.channel.send({embed}).catch(console.error());
+                        }
                     }
-                });
-            }
+                }
+            });
+        }
+    },
+    canRun: (client, messageUser, message) => {
+        if (message.channel.name === defaultConfig.channels.supportModding || message.channel.name === defaultConfig.channels.bots) {
+            return true;
         }
         else {
             let channels = [
                 client.channels.find("name", defaultConfig.channels.supportModding),
-                client.channels.find("name", defaultConfig.channels.bots)];
+                client.channels.find("name", defaultConfig.channels.bots)
+            ];
             message.channel.send(`Cette commande est seulement disponible dans les channels ${channels[0] + ", " + channels[1]}.`)
                 .then(async (message) => console.log(`Send message : ${message.content}`))
                 .catch(console.error());
+            return false;
         }
-    },
-    canRun: () => {
-        return true;
     }
 };
